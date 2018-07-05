92df0faebaddb9da8e205b5aefbd9a8088616e66
nickx360 <nickbetty2000@yahoo.com>
2018-07-05 23:24:11 +0530

    Swalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,  ElementRef, ViewChild,  AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit {
   title = 'app';
+  public columns = ['nick', 'tom','thomas','hamsa','helen'];
+  const length: number = this.columns.length;
+  @ViewChild('element') elementView;
+  @ViewChild('tableToMeasure') elementView2;
+
+    constructor() {
+    }
+
+    ngAfterViewInit() {
+        const sample = this.elementView.nativeElement.offsetWidth;
+        const sample2 = this.elementView2.nativeElement.offsetWidth;
+        if (sample2 < sample) {
+          let dest = sample2 / length;
+          console.log(dest);
+        }
+    }
+  }
 }
+
